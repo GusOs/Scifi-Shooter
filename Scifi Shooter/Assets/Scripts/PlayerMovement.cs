9e0ce4f9298084a4eb72781d82137ccb02e633fe
GusOs <gus_92@hotmail.es>
2020-09-04 16:42:24 +0200

    Add jump

--- Scifi Shooter/Assets/Scripts/PlayerMovement.cs
+++ Scifi Shooter/Assets/Scripts/PlayerMovement.cs
@@ -26,11 +26,17 @@ public class PlayerMovement : MonoBehaviour
     //Variable suelo del player
     private bool isGrounded;
 
+    //Salto
+    public float jumpHeight = 1f;
+
     //Variable para comprobar la distancia del suelo
     private float groundDistance = 0.35f;
 
     //Comprovación del suelo
     public LayerMask groundMask;
+
+    //Si salta
+    private bool isAbleToJump;
 
     void Start()
     {
@@ -51,6 +57,14 @@ public class PlayerMovement : MonoBehaviour
     {
         horizontalInput = Input.GetAxis("Horizontal");
         verticalInput = Input.GetAxis("Vertical");
+
+        if(Input.GetButtonDown("Jump"))
+        {
+            isAbleToJump = true;
+        }else
+        {
+            isAbleToJump = false;
+        }
     }
 
     private void CheckGround()
@@ -72,6 +86,11 @@ public class PlayerMovement : MonoBehaviour
         Vector3 movementDirection = Vector3.ClampMagnitude(forwardMovement + rightMovement, 1.0f);
 
         characterController.Move(movementDirection * playerSpeed * Time.deltaTime);
+
+        if(isAbleToJump)
+        {
+            velocity.y = Mathf.Sqrt(jumpHeight * -2f * -gravity);
+        }
 
         velocity.y -= gravity * Time.deltaTime;
         characterController.Move(velocity * Time.deltaTime);

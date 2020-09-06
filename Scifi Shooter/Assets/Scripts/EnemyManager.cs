c8112ec92c5325d916fc04290eb8615f14cbf35e
GusOs <gus_92@hotmail.es>
2020-09-06 15:53:44 +0200

    Add AI attempt

---
+++ Scifi Shooter/Assets/Scripts/EnemyManager.cs
@@ -0,0 +1,60 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyManager : MonoBehaviour
+{
+    //Transform del enemigo
+    public Transform player;
+
+    //Distancia de detección
+    public float distance = 15f;
+
+    //Animator del enemigo
+    public Animator anim;
+
+    public float enemyLife = 100f;
+
+    UnityEngine.AI.NavMeshAgent nav;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    /*Si la posición del jugador respecto al enemigo es menor que la distancia
+     * Siempre busca al jugador
+     * si la dirección es mayor de 5, cambia de animación
+    */
+    void Update()
+    {
+        if (Vector3.Distance(player.position, this.transform.position) < distance)
+        {
+            Vector3 direction = player.position - this.transform.position;
+            direction.y = 0;
+
+            this.transform.rotation = Quaternion.Slerp(this.transform.rotation, Quaternion.LookRotation(direction), 0.1f);
+
+            anim.SetBool("Idle_guard_AR", false);
+            if (direction.magnitude > 8)
+            {
+                nav = GetComponent<UnityEngine.AI.NavMeshAgent>();
+                nav.SetDestination(player.position);
+                anim.SetBool("Shoot_Autoshot_AR", true);
+            }
+            else
+            {
+                anim.SetBool("Run_guard_AR", false);
+            }
+        }
+        else
+        {
+            anim.SetBool("Idle_guard_AR", true);
+            anim.SetBool("Run_guard_AR", false);
+            anim.SetBool("Shoot_Autoshot_AR", false);
+        }  
+    }
+}
